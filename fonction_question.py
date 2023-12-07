836e5b9379dd8072244fb16ce3fcf7cd34d53137
PC Portable FRED <fredfredjalaguier@gmail.com>
2023-12-07 15:12:55 +0100

    token_question

--- fonction_question.py
+++ fonction_question.py
@@ -1,2 +1,27 @@
-def test():
-    pass
+def token_question(phrase):# retourne un tableau clean
+
+    # retire les majuscules :
+    phrase_min=""
+    for lettre in phrase :
+        if 65<=ord(lettre)<=90 :
+            phrase_min+=chr(ord(lettre)+32)
+        else :
+            phrase_min+=lettre
+
+    # retire la ponctuation
+    phrase_ponctu=""
+    for lettre in phrase_min :
+        mot=""
+        if 97<=ord(lettre)<=122 :
+            mot+=lettre
+        else :
+            mot+=" "
+        phrase_ponctu+=mot
+
+    #chaque mot est ajouté dans un tableau
+    tab_mot=phrase_ponctu.split()
+    return(tab_mot)
+
+
+
+print(token_question("salut C'est moi, fred."))
