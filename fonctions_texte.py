7221cba5dc241595034ce975e99d55ce96f9f857
Louis Gun <lgun1735@gmail.com>
2023-12-14 15:39:52 +0100

    nul devien 0 dans tf idf

--- fonctions_texte.py
+++ fonctions_texte.py
@@ -136,7 +136,7 @@ def TF_IDF(repertoire):
                 valeur_idf = idf.get(mot, 0)
                 valeur_tf = tf.get(mot, 0)
                 if valeur_tf==0:
-                    L.append('nul')
+                    L.append(0)
                 else:
                     L.append(valeur_tf * valeur_idf)
         matrice[mot]=L
@@ -336,12 +336,12 @@ sarkozy = 'Nomination_Sarkozy.txt'
 #TEST :
 # print(mot_dit("cleaned"))
 # print(nation("cleaned"))
-# print(mot_chirac())
+#print(mot_chirac())
 # print(convertir_minuscule(chirac1))
 # print(supprime_ponctuation(chirac1))
 # print(ajout_prenom("cleaned"))
 #print(TF_IDF("cleaned"))
-# print(mot_non_important("cleaned"))
-# print(plusgrand_TF_IDF("cleaned"))
+#print(mot_non_important("cleaned"))
+#print(plusgrand_TF_IDF("cleaned"))
 #print(IDF("cleaned"))
 
